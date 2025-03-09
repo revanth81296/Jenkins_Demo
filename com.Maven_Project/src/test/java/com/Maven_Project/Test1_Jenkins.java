359c599b81c363e8b4420ce4044d5db47585a0fe
Revanth S <30970292+revanth81296@users.noreply.github.com>
2025-03-09 12:12:47 +0530

    new TC Update

---
+++ com.Maven_Project/src/test/java/com/Maven_Project/Test1_Jenkins.java
@@ -0,0 +1,11 @@
+package com.Maven_Project;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1_Jenkins {
+	@Test
+	public void TC1() {
+		Reporter.log("Testing");
+	}
+}
